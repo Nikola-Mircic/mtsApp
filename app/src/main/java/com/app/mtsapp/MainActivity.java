cdc8cd025a71848a96112f0f329fba2e8bf0193b
Mrmi <nikola.31023@gmail.com>
2020-10-22 21:34:18 +0200

    Dodao dobijanje geografske visine i sirine preko gugl mapa koristeci Places API u Main Activity-u.

--- app/src/main/java/com/app/mtsapp/MainActivity.java
+++ app/src/main/java/com/app/mtsapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.app.mtsapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.mtsapp.location.LocationFinder;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -17,6 +23,11 @@ public class MainActivity extends AppCompatActivity{
 
     private TextView tvLatitude,tvLongitude;
     private Button getLocation;
+
+    //Mrmi - otvaranje preko gugl mapa
+    private Button googleMapsButton;
+    private TextView googleDetectedText;
+    private int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,9 @@ public class MainActivity extends AppCompatActivity{
         tvLatitude = (TextView) findViewById(R.id.tvLatitude);
         tvLongitude = (TextView) findViewById(R.id.tvLongitude);
         getLocation = (Button) findViewById(R.id.getLocation);
+
+        googleMapsButton = findViewById(R.id.googleMapsButton);
+        googleDetectedText = findViewById(R.id.googleDetectedText);
 
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +55,38 @@ public class MainActivity extends AppCompatActivity{
                 }
             }
         });
+
+        googleMapsButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PLACE_PICKER_REQUEST)
+        {
+            if(resultCode == RESULT_OK)
+            {
+                Place place = PlacePicker.getPlace(this, data);
+                String text;
+                String latitude = String.valueOf(place.getLatLng().latitude);
+                String longitude = String.valueOf(place.getLatLng().longitude);
+                text = "LATITUDE: " + latitude + "\nLONGITUDE: " + longitude;
+
+                googleDetectedText.setText(text);
+            }
+        }
     }
 
     @Override
