f541d091a74f719160dd25d720ea9aa317ec4113
Nikola165 <nikola123mircic@gmail.com>
2020-10-24 14:13:34 +0200

    Update LocationFinder.java

--- app/src/main/java/com/app/mtsapp/location/LocationFinder.java
+++ app/src/main/java/com/app/mtsapp/location/LocationFinder.java
@@ -3,6 +3,7 @@ package com.app.mtsapp.location;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,26 +14,32 @@ import com.google.android.gms.location.*;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class LocationFinder {
+public class LocationFinder implements Runnable{
+    private static final String TAG = "LocationFinder";//Tag koji se koristi za ispisivanje
+    private  AppCompatActivity activity;//Pamti vrednost aktivitija koji koristi location finder
 
-    private  AppCompatActivity activity;
+    private final int LOCATION_PERMISSSION_CODE = 100;//
 
-    private final int LOCATION_PERMISSSION_CODE = 100;
+    private FusedLocationProviderClient flpClient;//Objekat koji pokrece pretragu lokacije
+    private LocationRequest locationRequest;//Objekat koji salje zahtev za dobijanje lokacije i prosledjuje je do objekta povratnog poziva
+    private LocationCallback locationCallback;//Objekat koji obradjuje adresu kada je dobije
 
-    private FusedLocationProviderClient flpClient;
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
-
-    private Location currentLocation;
+    private Location currentLocation;//Trenutna lokacija
 
     public LocationFinder(AppCompatActivity activity){
-        this.activity = activity;
-        this.currentLocation = null;
+        this.activity = activity;//Pamti se gde je LocationFinder kreiran(u kom aktivitiju)
+        this.currentLocation = null;//Lokacija jos uvek nije pronadjena
 
+        Thread t = new Thread(this);//Kreira se nov zaseban proces za LocatinoFinder
+        t.start();
+    }
+
+    @Override
+    public void run() {
         flpClient = LocationServices.getFusedLocationProviderClient(this.activity);
         locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(1000);
+        locationRequest.setInterval(5000);
 
         locationCallback = new LocationCallback(){
             @Override
@@ -46,26 +53,30 @@ public class LocationFinder {
                 currentLocation = locationResult.getLastLocation();
             }
         };
+
+        for(int i=0;i<20;++i) {
+            Log.d(TAG, "sleeping...: "+i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        startLocating();
     }
 
     public void startLocating(){
         if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(activity,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             try {
-                Toast.makeText(activity, "Geting location...", Toast.LENGTH_SHORT).show();
                 flpClient.requestLocationUpdates(locationRequest, locationCallback, activity.getMainLooper());
-                flpClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                /*flpClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
                         currentLocation = location;
                     }
-                });
-                flpClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(activity, "Cann't get location...", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                });*/
             }catch (Exception e){
                 e.printStackTrace();
             }
@@ -78,7 +89,7 @@ public class LocationFinder {
     }
 
     public Location getCurrentLocation(){
-        return  this.currentLocation;
+        return  currentLocation;
     }
 
     public void stopLocating(){
